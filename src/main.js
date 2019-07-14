2d46ffae9b68015e172e10913ae9dcb6da2e846d
Yuriuh <642192014@qq.com>
2019-07-14 23:42:02 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,13 +9,12 @@ Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 Vue.directive('theme',{
-	bind(el,binding,vnode){
-		if (binding.value == 'wide') {
+	bind(el, binding, vnode){
+		if (binding.value === 'wide') {
 			el.style.maxWidth = "1260px";
 		} else if (binding.value == 'narrow') {
 			el.style.maxWidth = "560px";
 		}
-
 		if (binding.arg == 'column') {
 			el.style.background = "#6677cc";
 			el.style.padding = '20px';
